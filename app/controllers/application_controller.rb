c7a2f1e147ed97a641d2b1d59105144b19f97e7f
Nige <nigel.surtees@sage.com>
2014-04-01 15:28:07 +0100

    removed basic authentication:

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,4 @@
 class ApplicationController < ActionController::Base
-
-before_filter :authenticate
-
-  def authenticate
-    authenticate_or_request_with_http_basic('Administration') do |username, password|
-      username == 'admin' && password == 'password'
-    end
-  end
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
