879901d85fd1c65013061b04a23d3f38d7a2a31b
Nige <nigel.surtees@sage.com>
2014-04-13 22:43:51 +0100

    enabled user_id 1 and 2 to share categories and trantypes

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,6 +2,10 @@ class Category < ActiveRecord::Base
 	has_many :transactions
 	validates_presence_of :user_id
 	def self.user_categories(user_id)
-		Category.where(user_id: user_id)
+      if(user_id == 1 || user_id == 2)
+		Category.where(user_id: [1, 2])
+	  else
+	  	Category.where(user_id: user_id)
+	  end
 	end
 end
--- app/models/trantype.rb
+++ app/models/trantype.rb
@@ -3,6 +3,10 @@ class Trantype < ActiveRecord::Base
 	validates_presence_of :user_id
 
 	def self.user_trantypes(user_id)
-		Trantype.where(user_id: user_id)
+		if(user_id == 1 || user_id == 2)
+		  Trantype.where(user_id: [1, 2])
+	    else
+	  	  Trantype.where(user_id: user_id)
+	  end
 	end
 end
