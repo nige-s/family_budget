7e421cda40aa276e2cc5348518493f4b85049dc3
Nige <nigel.surtees@sage.com>
2014-04-17 17:32:51 +0100

    schema update

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140407184028) do
+ActiveRecord::Schema.define(version: 20140413123033) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 20140407184028) do
     t.string   "desc"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "recurring_transactions", force: true do |t|
@@ -100,6 +101,7 @@ ActiveRecord::Schema.define(version: 20140407184028) do
     t.string   "desc"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "users", force: true do |t|
