b802ac6244c02b81e2cf803dc21e8b61a4bca182
Nige <nigel.surtees@sage.com>
2014-04-21 10:50:53 +0100

    statement now shows correct balance

--- app/models/statement.rb
+++ app/models/statement.rb
@@ -5,6 +5,8 @@ class Statement < ActiveRecord::Base
   end
 	def self.account_balance(statement)
 	  tmp_account = Account.find(statement.acc_id)
-	  tmp_account.starting_balance + tmp_account.transactions.where("tran_date <= ?", statement.edate).sum(:amount)
+	  tmp_account.starting_balance + 
+	    (tmp_account.transactions.where("tran_date <= ?", statement.edate).where(sign: "credit").sum(:amount) -
+	     tmp_account.transactions.where("tran_date <= ?", statement.edate).where(sign: "debit").sum(:amount))
 	end
 end
